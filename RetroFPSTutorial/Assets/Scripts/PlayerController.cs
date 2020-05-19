4aab99cb4a6904198d6d66ad9067eb4de5d809b1
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-05-19 19:10:49 -0400

    mouse input

---
+++ RetroFPSTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float mouseSensitivity = 1;
+    public float moveSpeed = 5;
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 moveHorizontal = transform.up * -moveInput.x;
+        Vector3 moveVertical = transform.right * moveInput.y;
+
+        rb.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+
+        //player view control
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
+
+    }
+}
